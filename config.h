80731bb72fa987e0f91da1e61046ee439b884c89
ciceropaulino <workpaulinooliveira@gmail.com>
2024-05-12 23:55:01 -0300

    add patches: alpha and notitle

--- config.h
+++ config.h
@@ -7,15 +7,24 @@ static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
 static const char dmenufont[] = "monospace:size=10";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
+static const char col_gray1[] = "#000000"; /*black bg normal mod*/
+static const char col_gray2[] = "#000000"; /*black bg border*/
+static const char col_gray3[] = "#444444";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_cyan[] = "#ff0000"; /*red bg selctor and selected window*/
+// static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0x00U;     /*alpha patch variables*/
+static const unsigned int borderalpha = OPAQUE; /*alpha patch variables*/
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
     [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+};
+
+static const unsigned int alphas[][3] = {
+    /*               fg      bg        border*/
+    [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeSel] = {OPAQUE, OPAQUE, borderalpha},
 };
 
 static const char *const autostart[] = {
@@ -110,7 +119,6 @@ static const Button buttons[] = {
     /* click                event mask      button          function argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
     {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
     {ClkClientWin, MODKEY, Button1, movemouse, {0}},
     {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
